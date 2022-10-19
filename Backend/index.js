b85637b5e8bcdbe78bdf1c52060e80e7164e5687
Imran Khan Mohammad <92031287+imrankhmd@users.noreply.github.com>
2022-10-19 16:27:18 +0530

    backend code updates

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+
+const express = require("express")
+const connection = require("./db/db")
+const authRouter = require("./Routes/auth")
+const cors = require("cors")
+const bmiRouter=require("./Routes/bmi.routes")
+
+const app = express()
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+app.use(cors({
+    origin:["*"]
+}))
+app.use("/auth", authRouter)
+app.use("/bmi.routes",bmiRouter)
+
+app.get("/", (req,res)=>{
+    res.send(`
+    <h2>
+    Home Page
+    </h2>
+    `)
+})
+
+app.listen(8080, async()=>{
+    await connection;
+    console.log("connected to db");
+    console.log("server started on http://localhost:8080");
+})
